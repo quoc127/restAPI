1f19130ba19b9ec1c05bbc53f80ce449892bb063
quoc127 <nquoc160200@gmail.com>
2022-03-22 10:20:45 +0700

    rest API update

---
+++ src/main/java/com/in28minutes/springboot/rest/example/student/StudentNotFoundException.java
@@ -0,0 +1,25 @@
+//package com.in28minutes.springboot.rest.example.student;
+//
+//public class StudentNotFoundException extends RuntimeException {
+//	
+//	/**
+//	 * 
+//	 */
+//	private static final long serialVersionUID = 1L;
+//
+//	public StudentNotFoundException(String exception) {
+//		super(exception);
+//	}
+//
+//}
+
+
+package com.in28minutes.springboot.rest.example.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/springboot/rest/example/SpringBoot2RestServiceBasicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28minutes.springboot.rest.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringBoot2RestServiceBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
